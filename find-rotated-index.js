ada4168fd19be24fdb346ad4d1c82c11a5b26798
Olivia Golden <olivia.golden.w@gmail.com>
2024-01-30 20:27:15 -0600

    find rotated index

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,29 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex(array, target) {
+    var leftIndex = 0;
+    var rightIndex = array.length - 1;
+    while (leftIndex <= rightIndex) {
+		var midIndex = leftIndex + Math.floor((rightIndex - leftIndex) / 2);
+        if (array[midIndex] === target) {
+            return midIndex;
+        }
+        else if (array[midIndex] < target) {
+            if (array[rightIndex] < target) {
+                rightIndex = midIndex - 1;
+            }
+            else {
+                leftIndex = midIndex + 1;
+            }
+        }
+        else {
+            if (array[leftIndex] > target) {
+                leftIndex = midIndex + 1;
+            }
+            else {
+                rightIndex = midIndex - 1;
+            }
+        }
+    }
+    return -1;
 }
 
 module.exports = findRotatedIndex
