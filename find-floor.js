2d25e455f81923879bbd3a16c4aa89cb53ed0bcb
Olivia Golden <olivia.golden.w@gmail.com>
2024-01-30 20:50:44 -0600

    find floor

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,37 @@
-function findFloor() {
-  
+function findFloor(array, target) {
+    console.log("start");
+    var leftIndex = 0;
+    var rightIndex = array.length - 1;
+    while (leftIndex <= rightIndex) {
+		var midIndex = leftIndex + Math.floor((rightIndex - leftIndex) / 2);
+        console.log(array[midIndex]);
+        if (array[midIndex] === target) {
+            return target;
+        }
+        else if (array[midIndex] < target) {
+            if (array[midIndex + 1] === target) {
+                return array[midIndex + 1];
+            }
+            else if (array[midIndex + 1] > target || array[midIndex + 1] === undefined) {
+                return array[midIndex];
+            }
+            else {
+                leftIndex = midIndex + 1;
+            }
+        }
+        else {
+            if (array[midIndex - 1] === target) {
+                return array[midIndex - 1];
+            }
+            else if (array[midIndex - 1] < target) {
+                return array[midIndex];
+            }
+            else {
+                rightIndex = midIndex - 1;
+            }
+        }
+    }
+    return -1;
 }
 
 module.exports = {
--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,23 @@
-function findRotationCount() {
-  
+function findRotationCount(array) {
+    var leftIndex = 0;
+    var rightIndex = array.length - 1;
+    var min = array[leftIndex];
+    while (leftIndex <= rightIndex) {
+		var midIndex = leftIndex + Math.floor((rightIndex - leftIndex) / 2);
+        if (array[midIndex] < min) {
+            min = array[midIndex];
+            if (array[midIndex - 1] < min) {
+                rightIndex = midIndex - 1;
+            }
+            else {
+                return midIndex;
+            }
+        }
+        else {
+            leftIndex = midIndex + 1;
+        }
+    }
+    return 0;
 }
 
 module.exports = findRotationCount
